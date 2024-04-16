b095922df223105d74d17798c4811704f167eece
StaphonP <SPeterson@appdev.yearup.org>
2024-04-16 10:26:20 -0400

    AddressBuilder

---
+++ workbook-2/com/pluralsight/AddressBuilder.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class AddressBuilder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String billingStreet, billingCity , billingState;
+        String billingZip;
+        System.out.println("Enter Full Name: ");
+        String name = input.nextLine();
+        System.out.println("Billing Street: ");
+        billingStreet = input.nextLine();
+        System.out.println("Billing City: ");
+        billingCity = input.nextLine();
+        System.out.println("Billing State: ");
+        billingState = input.nextLine();
+        System.out.println("Billing Zip");
+        billingZip = input.nextLine();
+     String shippingStreet,shippingCity,shippingState;
+        System.out.println("Now enter Shipping Address.");
+     String shippingZip;
+        System.out.println("Shipping Street: ");
+        shippingStreet = input.nextLine();
+        System.out.println("Shipping City: ");
+        shippingCity = input.nextLine();
+        System.out.println("Shipping State: ");
+        shippingState = input.nextLine();
+        System.out.println("Shipping Zip");
+        shippingZip = input.next();
+         StringBuilder billingAddress = new StringBuilder();
+                 billingAddress.append(billingStreet+"\n");
+                 billingAddress.append(billingCity+", ");
+                 billingAddress.append(billingState+" ");
+                 billingAddress.append(billingZip);
+                 String billing = billingAddress.toString();
+
+                 StringBuilder shippingAddress = new StringBuilder();
+                 shippingAddress.append(shippingStreet+"\n");
+                 shippingAddress.append(shippingCity+",");
+                 shippingAddress.append(shippingState+" ");
+                 shippingAddress.append(shippingZip);
+                 String shipping = shippingAddress.toString();
+
+                 System.out.println("Full Name: "+name);
+                 System.out.println("Billing Address:\n "+billing);
+                 System.out.println("Shipping Address:\n "+shipping);
+
+
+
+
+    }
+
+}
