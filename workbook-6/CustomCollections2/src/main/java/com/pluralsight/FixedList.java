9931a79e9818402994000a71a9a5e6f76478fbcd
StaphonP <SPeterson@appdev.yearup.org>
2024-05-22 11:10:20 -0400

    Updated

--- workbook-6/CustomCollections2/src/main/java/com/pluralsight/FixedList.java
+++ workbook-6/CustomCollections2/src/main/java/com/pluralsight/FixedList.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FixedList <T> {
-    private List<T> items = new ArrayList<T>();
+    private List<T> items;
     private int maxSize;
     public FixedList(int maxSize){
-        this.maxSize = 4;
+        this.maxSize = maxSize;
+         items = new ArrayList<T>();
     }
     public void add(T item){
         if (items.size() < maxSize){
-            add(item);
+            items.add(item);
         } else if(items.size() == maxSize){
             System.out.println("Cannot add "+ item+ ", List Full");
         }
     }
 
     public List<T> getItems() {
+
         return items;
     }
 }
--- workbook-6/CustomCollections2/src/main/java/com/pluralsight/FixedListDriver.java
+++ workbook-6/CustomCollections2/src/main/java/com/pluralsight/FixedListDriver.java
@@ -1,15 +1,21 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+
 public class FixedListDriver {
     public static void main(String[] args) {
-      /*  FixedList<Integer> numbers = new FixedList<Integer>(4);
+       FixedList<Integer> numbers = new FixedList<Integer>(4);
         numbers.add(23);
         numbers.add(43);
         numbers.add(32);
         numbers.add(9);
         numbers.add(60);
-      */
-      //  System.out.println(numbers.getItems().size());
-        System.out.println("Hello");
+
+      System.out.println(numbers.getItems().size());
+FixedList<LocalDate> dates = new FixedList<LocalDate>(2);
+dates.add(LocalDate.now());
+dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+        System.out.println(dates.getItems().size());
     }
 }
